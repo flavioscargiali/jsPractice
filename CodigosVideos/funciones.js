1351230ba15e74d3e173babd61e15eba6bd54ecc
Flavio Scargiali <flavio.scargiali@gmail.com>
2025-02-17 00:10:17 -0300

    Códigos del video de funciones

---
+++ CodigosVideos/funciones.js
@@ -0,0 +1,85 @@
+// Funciones 
+
+// empieza con la palabra function
+// le damos nombre a la función 
+// () establece que es una función 
+// {} dentro está el código de la función 
+
+function sayHello(){
+    console.log("Hello World");
+}
+
+sayHello(); 
+
+// Las funciones se pueden categorizar en funciones parametrizadas y funciones no parametrizadas.
+// No parametrizadas 
+function addNP(){
+    const a = 3; 
+    const b = 4;
+    return a + b;
+}
+
+document.getElementById("functiondata1").innerText = addNP(); 
+
+// Parametrizadas incluye inputs (argumentos) 
+function addP(a,b){
+
+    return a + b;
+    
+}
+
+document.getElementById("functiondata2").innerText = addP(3,4); 
+
+// Parametros son variables definidas en la declaración dentro de la función
+// Arguments: son los valores que le pasamos
+
+// Dos maneras diferentes de escribir funciones: function declaration (tradicional) y function expression 
+
+
+// Types of functions
+// Named functions
+
+const add = function(a,b){
+    console.log(a+b);
+}
+
+add(2, 3);
+
+// IIFE: definidas y ejecutadas inmediatamente 
+(function sayHelloWorld(){
+    console.log("Hello")
+})();
+
+// Arrow function
+const square = (x) => x*x; 
+
+// Anonymous function
+
+// ----------------------------------
+// ECMA
+// Arrow function
+// (a,b) son los parametros
+// a*b el cuerpo de la función
+const multiply = (a,b) => a*b;
+
+// Se pueden clasificar en funciones parametrizadas y funciones no parametrizadas
+// Parametrizadas
+const addArrow = (a,b) => a+b;
+console.log(addArrow(3,4));
+
+// No parametrizadas 
+const greet = () => {
+    console.log("Hello, World!")
+};
+
+greet(); 
+
+// Return statemente es aquello que indicamos que sea devuelto por la función. 
+const addReturn = function(a,b){
+    return a+b;
+}
+
+const resultado = addReturn(2, 3);
+
+// El return especifica cual es el ouput de la función. 
+document.getElementById("functiondata3").innerText = addReturn(3,10);
